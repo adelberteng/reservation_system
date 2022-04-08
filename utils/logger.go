322e6a45c84f041946356db5d1f0f401fb02c64d
Albert <adelberteng@gmail.com>
2022-04-08 17:54:28 +0800

    use sync.Once to make logger singleton.

--- utils/logger.go
+++ utils/logger.go
@@ -6,24 +6,26 @@ import (
 	"os"
 )
 
-var Logger = logrus.New()
+var Logger logrus.Logger
 
 func init() {
 	logDir := Config.Log.Dir
 	logName := Config.Log.FileName
 	logLevel := Config.Log.Level
 
-	os.MkdirAll(logDir, 0766)
-	logFile, err := os.OpenFile(logDir+"/"+logName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		Logger.Fatalf("log file open error : %v", err)
-	}
-
-	Logger.Out = io.MultiWriter(os.Stdout, logFile)
-	Logger.Formatter = &logrus.JSONFormatter{}
-	logrusLevel, err := logrus.ParseLevel(logLevel)
-	if err != nil {
-		Logger.Fatalf("log level error : %v", err)
-	}
-	Logger.Level = logrusLevel
+	once.Do(func() {
+		Logger = *logrus.New()
+		os.MkdirAll(logDir, 0766)
+		logFile, err := os.OpenFile(logDir+"/"+logName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			Logger.Fatalf("log file open error : %v", err)
+		}
+		Logger.Out = io.MultiWriter(os.Stdout, logFile)
+		Logger.Formatter = &logrus.JSONFormatter{}
+		logrusLevel, err := logrus.ParseLevel(logLevel)
+		if err != nil {
+			Logger.Fatalf("log level error : %v", err)
+		}
+		Logger.Level = logrusLevel
+	})
 }
