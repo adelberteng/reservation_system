a0fc25833a11a675f49787a46f6d14d32766d224
Albert <adelberteng@gmail.com>
2022-04-09 23:08:42 +0800

    add handler in place.

--- handlers/place.go
+++ handlers/place.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddPlace(c *gin.Context) {
+	var json map[string]string
+	c.ShouldBindJSON(&json)
+
+	// placeName := json["place_name"]
+	// region := json["region"]
+	// placeType := json["place_type"]
+	// address := json["address"]
+	// capacity := json["capacity"]
+
+
+}
